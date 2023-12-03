c9b98ce406ba0f1da8ac59b58789837013ae57d3
Miroslav Babkov <miroslavbabk@gmail.com>
2023-12-03 15:47:59 +0300

    Update main.py
    The trick of this project is not its complexity, but its usefulness and history.
    
    I had an entrance exam at school, I wanted to write this exam for maximum score, but I could not understand how to solve one example, but I had already written something python, I did not understand how to solve this example, I wrote a code that solves this example by substitution and wrote a bot that I helped me to solve this example. I ran a local server and the bot and this bot helped me pass the paper with a maximum score.

--- main.py
+++ main.py
@@ -11,20 +11,11 @@ def cmd_start(message):
     v1 = bot.send_message(message.from_user.id, 'Напиши два числа через запятую: ')
     bot.register_next_step_handler(v1, sosi)
 
-
-
 def sosi(mes):
     v2 = mes.text
     v2 = v2.split(',')
     s = 2 * int(v2[0]) * int(v2[1])
     bot.send_message(mes.chat.id, s / (int(v2[0]) + int(v2[1])))
-
-
-
-
-
-
-
 
 if __name__ == "__main__":
     bot.polling()
